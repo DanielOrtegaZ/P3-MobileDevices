c72361c910c2fe4567b2b774e12cb291ff7173fb
DanielOrtegaZ <dortegaz1500@alumno.ipn.mx>
2018-10-27 02:34:58 -0500

    Practice Finished (Includes P1)

---
+++ app/src/main/java/a3cv6/escom/ipn/mx/login/Perfil.kt
@@ -0,0 +1,18 @@
+package a3cv6.escom.ipn.mx.login
+
+import android.app.Activity
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class Perfil : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil)
+
+        val name : TextView = findViewById<TextView>(R.id.nombre)
+        name.text = "Bienvenido " + intent.getStringExtra("Nombre");
+    }
+
+}
